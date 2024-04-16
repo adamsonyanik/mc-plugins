f3cbf8bb213052eda6381dbab1dac554269b9a1f
David Adamson <david.adamson2304@gmail.com>
2024-04-16 18:06:44 +0200

    gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,39 @@
 plugins {
+    java
     kotlin("jvm") version "1.9.23"
+    id("me.qoomon.git-versioning") version "6.4.3"
 }
 
 group = "io.github.adamsonyanik"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+version = "0.0.0-SNAPSHOT"
+gitVersioning.apply {
+    refs {
+        branch(".+") {
+            version = "\${ref}-\${commit}-SNAPSHOT"
+        }
+        tag("v(?<version>.*)") {
+            version = "\${ref.version}"
+        }
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://oss.sonatype.org/content/repositories/central")
+    }
+
+    dependencies {
+        "compileOnly"("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+    }
 }
