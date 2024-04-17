6c61e823334f8be0fbcf0ec2d48b1436e0ebf530
David Adamson <david.adamson2304@gmail.com>
2024-04-17 13:24:09 +0200

    fix: remove kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import kr.entree.spigradle.kotlin.spigot
 
 plugins {
     java
-    kotlin("jvm") version("1.9.23")
     id("kr.entree.spigradle") version("2.4.3") apply(false)
 }
 
@@ -13,7 +12,6 @@ java {
 
 subprojects {
     apply(plugin = "java")
-    apply(plugin = "kotlin")
     apply(plugin = "kr.entree.spigradle")
 
     group = "io.github.adamsonyanik"
@@ -24,7 +22,6 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
         compileOnly(spigot("1.20.4-R0.1-SNAPSHOT"))
     }
 }
