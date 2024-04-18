0c6553280698f40f349b95c4e84555614a6aabdd
David Adamson <david.adamson2304@gmail.com>
2024-04-18 09:10:31 +0200

    feat: implement autoreload plugin

--- plugin-autoreload/src/main/java/io/github/adamsonyanik/DirectoryWatcher.java
+++ plugin-autoreload/src/main/java/io/github/adamsonyanik/DirectoryWatcher.java
@@ -1,6 +1,7 @@
 package io.github.adamsonyanik;
 
 import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.StandardWatchEventKinds;
@@ -24,24 +25,45 @@ public class DirectoryWatcher {
     this.subscribers.add(callback);
   }
 
-  public void stop() throws IOException {
-    this.watchService.close();
+  public synchronized void stop() {
+    if (this.watchService == null) throw new IllegalStateException("watcher not started");
+    try {
+      this.watchService.close();
+      this.watchService = null;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  public void start() {
-    this.watchService = FileSystems.getDefault().newWatchService();
-    this.directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
-        StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
-    while (true) {
-      WatchKey key = watchService.take();
-      Path path = (Path) key.watchable();
-      for (var event : key.pollEvents()) {
-        var file = path.resolve((Path) event.context());
-        for (var subscriber : this.subscribers) {
-          subscriber.accept(file);
+  public synchronized void start() {
+    if (this.watchService != null) throw new IllegalStateException("watcher already started");
+    try {
+      this.watchService = FileSystems.getDefault().newWatchService();
+      this.directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
+          StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    new Thread(this::pollUntilStop).start();
+  }
+
+  private void pollUntilStop() {
+    try {
+      while (true) {
+        WatchKey key = watchService.take();
+        Path path = (Path) key.watchable();
+        for (var event : key.pollEvents()) {
+          var file = path.resolve((Path) event.context());
+          for (var subscriber : this.subscribers) {
+            subscriber.accept(file);
+          }
         }
+        key.reset();
       }
-      key.reset();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } catch (ClosedWatchServiceException e) {
+      // do nothing. expect that the watch service was gracefully closed.
     }
   }
 }
--- plugin-autoreload/src/main/java/io/github/adamsonyanik/MCAutoReload.java
+++ plugin-autoreload/src/main/java/io/github/adamsonyanik/MCAutoReload.java
@@ -5,25 +5,29 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCAutoReload extends JavaPlugin {
+    private DirectoryWatcher watcher;
+
     @Override
     public void onEnable() {
         var plugins = Paths.get(this.getDataFolder().getAbsolutePath()).getParent();
-        var watcher = new DirectoryWatcher(plugins);
-        watcher.onFileChanged(System.out::println);
-        watcher.start();
-
-        new Thread(() -> {
-          try {
-            watcher.start();
-          } catch (Exception e) {
-            throw new RuntimeException(e);
+        this.watcher = new DirectoryWatcher(plugins);
+        watcher.onFileChanged((path) -> {
+          var file = path.getFileName();
+          if (file.toString().endsWith(".jar")) {
+            Bukkit.broadcastMessage("[plugin-autoreload]: detected change in: " + file);
+            Bukkit.getScheduler().cancelTasks(this);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
+              Bukkit.broadcastMessage("[plugin-autoreload]: performing reload ...");
+              Bukkit.reload();
+              Bukkit.broadcastMessage("[plugin-autoreload]: reload complete");
+            }, 20);
           }
-        }).start();
+        });
+        watcher.start();
     }
 
-    private static void rl() {
-        Bukkit.broadcastMessage("reloading...");
-        Bukkit.reload();
-        Bukkit.broadcastMessage("reload complete");
+    @Override
+    public void onDisable() {
+        this.watcher.stop();
     }
 }
